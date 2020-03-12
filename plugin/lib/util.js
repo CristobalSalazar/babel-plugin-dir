75c534b517d998e5e1d3bf5fdaf0e868bb0b6253
CristobalSalazar <chris.salza@gmail.com>
2020-03-11 22:54:51 -0400

    format

--- plugin/lib/util.js
+++ plugin/lib/util.js
@@ -13,11 +13,13 @@ module.exports = {
       return fileName.substr(0, dotIndex);
     }
   },
+
   getValidFilesFromDirectory(dir, regx) {
     return fs.readdirSync(dir, "utf-8").filter(file => {
       return regx.test(file);
     });
   },
+
   getAbsoluteImportDirectoryPath(babelFilePath, importPath) {
     const importDir = path.dirname(importPath);
     const currentDirPath = path.dirname(babelFilePath);
